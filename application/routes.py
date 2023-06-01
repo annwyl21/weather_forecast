83a138702162a5a44424e38347bf316591e04295
Ellen Houghton <ellen.a.ash@gmail.com>
2023-06-01 16:16:43 +0100

    add button for 2nd location

--- application/routes.py
+++ application/routes.py
@@ -7,7 +7,11 @@ from application.form import LocationForm
 def index():
     return render_template('index.html', title='Line-dried Laundry')
 
-@app.route('/detail')
-def detail():
-    weather_forecast = Rain(get_weather_data('london'))
-    return render_template('detail.html', title='London Weather', weather_forecast=weather_forecast)
+@app.route('/<location>')
+def detail(location):
+    if location == 'london':
+        weather_forecast = Rain(get_weather_data('london'))
+        return render_template('detail.html', title='London Weather', location='London', weather_forecast=weather_forecast)
+    elif location == 'inverness':
+        weather_forecast = Rain(get_weather_data('inverness'))
+        return render_template('detail.html', title='Inverness Weather', location='Inverness', weather_forecast=weather_forecast)
